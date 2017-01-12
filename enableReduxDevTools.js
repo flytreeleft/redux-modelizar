050667afa197c705207e99d4d101fd933542156a
flytreeleft <flytreeleft@126.com>
2017-01-12 09:34:49 +0800

    redux-modelizar: adapt to Redux Dev Tools v2.12+

--- enableReduxDevTools.js
+++ enableReduxDevTools.js
@@ -8,6 +8,15 @@ export default function enableReduxDevTools() {
     var extension = window.__REDUX_DEVTOOLS_EXTENSION__ || window.devToolsExtension;
 
     return extension ? extension({
+        // Adapt to Redux Dev Tools v2.12+
+        serialize: {
+            // Convert to JSON object
+            replacer: (key, value) => isState(value) ? value.valueOf() : value,
+            // Convert to State object
+            reviver: (key, value) => isState(value) ? value : createState(value)
+        },
+        // NOTE: In the latest version, (de)serializeState are replaced by serialize
+        // See https://github.com/zalmoxisus/redux-devtools-extension/blob/master/docs/API/Arguments.md
         deserializeState: (state) => isState(state) ? state : createState(state),
         serializeState: (key, value) => isState(value) ? value.valueOf() : value
     }) : (f) => f;
