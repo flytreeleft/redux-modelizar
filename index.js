780c4e30cd9961c65b60b67e78f86966dbd5c272
flytreeleft <flytreeleft@126.com>
2017-03-16 18:46:02 +0800

    redux-modelizar: remove useless exported modules

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 export {
-    REDUX_MODELIZAR_NAMESPACE
+    REDUX_MODELIZAR
 } from './namespace';
-export {
-    MODEL_STATE_MUTATE
-} from './modelizar/actions';
 
-export {
-    createState
-} from './state';
 export {
     default as createStore
 } from './createStore';
@@ -21,38 +15,12 @@ export {
     default as applyMiddleware
 } from './applyMiddleware';
 export {
-    default as mappingState
-} from './mappingState';
-export {
     default as enableReduxDevTools
 } from './enableReduxDevTools';
 
 export {
-    default as modelizar
-} from './modelizar';
-export {
     default as undoable
 } from './undoable';
-
 export {
-    registerFunction
-} from './object/functions';
-export {
-    default as toPlain
-} from './object/toPlain';
-export {
-    default as toReal
-} from './object/toReal';
-export {
-    default as syncReal
-} from './object/syncReal';
-export {
-    default as diffReal
-} from './object/diffReal';
-export {
-    default as proxy,
-    isProxied
-} from './object/proxy';
-export {
-    default as guid
-} from './utils/guid';
+    default as bindHistory
+} from './undoable/bindHistory';
